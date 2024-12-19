1094902308ae6ce7f21757715c244ca9cd03b353
victorpaularony <smallypauls@gmail.com>
2024-12-19 15:10:01 +0300

    chore: added a receive function to the simple banking

--- simpleBank/simpleBank.sol
+++ simpleBank/simpleBank.sol
@@ -40,23 +40,53 @@ contract SimpleBank{
         msg.value > 0 ,
         "The amount to be deposited should not be less than 0: "
     );
-    balance += msg.value;
+    balance[msg.sender] += msg.value;
     emit Deposit(msg.sender, msg.value);
     }
 
     //function to allow withdrawal of funds 
-    function withdrawal(uint amount) public {
+    function withdrawal(uint _amount) public {
         require(
             msg.sender == owner,
             "Only the ower can withdarw funds from their accounts"
         );
         require(
-            amount >0 && amount <= balance,
+            balance[msg.sender] >= _amount,
             "The amount is too much or to low to withdarw: "
         );
-        balance -= amount;
-        owner.transfer(amount);
-        emit Withdrawal(msg.sender, amount);
+        
+        emit Withdrawal(msg.sender, _amount);
+    }
+
+    //function to allow the user to transfer funds to other users
+    function transfer(address payable _to, uint _amount)public{
+        require(
+            balance[msg.sender] >= _amount,
+            "You have insuffcient amount in your account to transfer"
+        );
+        require(
+            owner == msg.sender,
+            "Only owner can transfer funds to another"
+        );
+        balance[msg.sender] -= _amount;
+        balance[_to] += _amount;
+
+        emit Transfer(msg.sender, _to, _amount);
+    }
+
+    //function to enable owner to check balance
+    function getBalance() public view returns(uint){
+        return balance[msg.sender];
+    }
+
+    //function fallback that accept Ether sent to the contract direcly
+    fallback()external payable{
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    //function receive that is called if data is not sent with the transaction
+    receive() external payable{
+        emit Deposit(msg.sender, msg.value);
     }
 
 }
