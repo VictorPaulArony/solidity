ff5a6677b1dd32d71b5ae79eb7eb319f1446bb86
victorpaularony <smallypauls@gmail.com>
2025-01-10 23:27:18 +0300

    loops/whileloop.sol

--- loops/whileloop.sol
+++ loops/whileloop.sol
@@ -5,12 +5,15 @@ pragma solidity ^0.8.0;
 contract WhileLoop {
     uint public total;
 
-    //function to add nums while the value is above the condition
+     //function to add nums while the value is above the condition
     function addWhile(uint _num) public {
         total = 0;
-        while (total < _num) {
-            total += _num;
+        uint i = 0;
+        while(i < _num) {
+            total+= i;
+            i++;
         }
+        
     }
 
     function getTotalSum() public view returns (uint) {
@@ -21,14 +24,14 @@ contract WhileLoop {
     uint[] public arr;
 
     //function to add numbers in an array as long as the condition is valid
-    function addToArray(uint _num) public returns (uint[] memory) {
+    function addToArray(uint _num) public {
         uint count = 0;
 
         do {
             arr.push(count);
             count++;
         } while (count < _num);
-        return arr;
+    
     }
 
     //function to check the lenth of the array
